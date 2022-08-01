ced4b9c1da404b9dc1043c354fa82eb55edd8103
LiYiJun <liyijundashuaibi@gmail.ccom>
2022-08-01 15:07:48 +0800

    first coommit

---
+++ backend/dummydata/articles.js
@@ -0,0 +1,41 @@
+const articles = [
+{
+    _id:"1",
+    title:"An experimental evaluation of test driven development vs. test-last development with industry professionals",
+    authors:"Munir, H.,Wnuk,K.,Petersen, K.,Moayyed,M.",
+    source:"EASE",
+    pubyear:"2014",
+    doi:"https://doi.org/10.1145/2601248.2601267",
+    claim_evidance:[["code improvement","strong support"],["product improvement","weak against"],["team improvement","none"]],
+},
+{
+    _id:"2",
+    title:"Realizing quality improvement throough test driven development: results and experiencces of four industrial teams",
+    authors:"Nagappan,N.,Maximilien,E.M.,Bhat,T.,Williams,L.",
+    source:"Empirical Software Engineering,13(3),289-302",
+    pubyear:"2008",
+    doi:"https://doi.org/10.1007/s10664-0008-9062-z",
+    claim_evidance:[["code improvement","weak support"],["product improvement","weak against"],["team improvement","low support"]],
+},
+{
+    _id:"3",
+    title:"Dose Test-Driven Development Really Improve Software Design Quality?",
+    authors:"Janzen,D.S.",
+    source:"Softwaare,IEEE.,25(2)77-84",
+    pubyear:"2008",
+    doi:"",
+    claim_evidance:[["code improvement","strong support"],["product improvement","weak support"],["team improvement","none"]],
+},
+{
+    _id:"4",
+    title:"A Comparative Caase Study on the Impact of Test-Driven Development on Program Design and Test Coverage",
+    authors:"Siniaalto,M.,Abrahamsson, P.",
+    source:"ArXiv.org,cs.SE,ArXiv:1711.05082-284",
+    pubyear:"2017",
+    doi:"https://doi.org/10.1109/esem.2007.35",
+    claim_evidance:[["code improvement","weak support"],["product improvement","weak support"],["team improvement","none"]],
+},
+
+];
+
+module.exports = articles;
